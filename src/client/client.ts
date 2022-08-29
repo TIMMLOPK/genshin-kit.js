d4bd43bb8739de26a6b463232eb88f1dc6c244ae
TIMMYLOPK <timmy.12345wer11@gmail.com>
2022-08-29 20:26:44 +0800

    export full api

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,8 +1,10 @@
 import type { ClientCookieManager } from "./clientCookieManager";
 import { getAbyssBattle } from "../api/abyssAPI";
 import { getGameRecordCard } from "../api/RecordCard";
+import { getFullBattle } from "../api/full";
 import type { Base } from "../interface/base";
 import type { AbyssBattle } from "../interface/abyss";
+import type { Full } from "../interface/full";
 
 
 export class Client {
@@ -47,6 +49,18 @@ export class Client {
         const res = new getGameRecordCard().requestAPI(language, `ltoken=${ltoken}; ltuid=${ltuid};`, uid);
         return res;
     }
+
+    /**
+     * @param {string} uid - Game uid.
+     */
+    public async getFullBattle(uid: string): Promise<Full> {
+        const ltoken = this.cookieManager.ltoken;
+        const ltuid = this.cookieManager.ltuid;
+        const { language } = this.options;
+        const res = new getFullBattle().requestAPI(language, `ltoken=${ltoken}; ltuid=${ltuid};`, uid);
+        return res;
+    }
+
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./api/abyssAPI";
 export * from "./api/RecordCard";
+export * from "./api/full";
 
 export * from "./client/clientCookieManager";
 export * from "./client/client";
