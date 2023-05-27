ec649df766969a4c31e5e779b4fd73b362e4f170
TIMMYLOPK <timmy.12345wer11@gmail.com>
2023-05-27 12:12:48 +0800

    chore: run format

--- packages/genshin-kit.js/src/interface/shared.ts
+++ packages/genshin-kit.js/src/interface/shared.ts
@@ -1,12 +1,12 @@
 export interface Time {
-    year: number;
-    month: number;
-    day: number;
-    hour: number;
-    minute: number;
-    second: number;
+  year: number;
+  month: number;
+  day: number;
+  hour: number;
+  minute: number;
+  second: number;
 }
 
 export type KeyToUpperCase<T> = {
-    [K in keyof T as Capitalize<string & K>]: T[K];
+  [K in keyof T as Capitalize<string & K>]: T[K];
 };
